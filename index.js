2b0b2db4cca686c590488ad77d3e6ebe456f8a55
sBerger <stephone13@gmail.com>
2020-10-23 14:26:32 +0200

    Jeu Simon ok

---
+++ index.js
@@ -0,0 +1,71 @@
+const vm = new Vue({
+
+    el:'#app',
+    data:{
+        hautGauche:false,
+        hautDroite:false,
+        basGauche:false,
+        basDroite:false,
+        sequence:[],
+        tmp:[],
+        squareMapping:['hautGauche','hautDroite','basGauche','basDroite'],
+        score:0
+    },
+    methods:{
+        allGray(){
+            this.hautGauche = false;
+            this.hautDroite = false;
+            this.basGauche = false;
+            this.basDroite = false;
+        },
+        addNewElemToSequence(){
+            this.sequence.push(this.squareMapping[Math.floor(Math.random() * 4)]);
+            this.tmp = this.sequence.slice();
+        },
+        newGame(){
+            this.score = 0;
+            this.sequence = [];
+            this.nextTurn();
+
+        },
+        nextTurn(){
+            this.score+=this.sequence.length;
+            this.addNewElemToSequence();
+            this.allGray();
+            this.playSequence(this.tmp[0]);
+        },
+        playSequence(instruction){
+            this[instruction] = true;
+            setTimeout(function(){
+                vm.allGray();
+                vm.tmp.shift();
+                if(vm.tmp[0]){
+                    setTimeout(function(){
+                        vm.playSequence(vm.tmp[0]);
+                    },400);
+                } else {
+                    vm.tmp = vm.sequence.slice();
+                }
+            },400);
+        },
+        instruction(instruction){
+            if(instruction === this.tmp[0]){
+                this[instruction] = true;
+                setTimeout(function(){
+                    vm.allGray();
+                    vm.tmp.shift();
+                    if(!vm.tmp[0]){
+                        setTimeout(() => {
+                            vm.nextTurn();
+                        }, 400);
+                    }
+                },400)
+            } else {
+                alert(`Perdu!!, Votre score est de: ${this.score}`);
+                this.score = 0;
+                this.sequence = [];
+                this.tmp = []
+            }
+        }
+    }
+})
